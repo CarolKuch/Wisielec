881f4abecf62a47b100dedc7a1a1f5c79e824669
CarolKuch <kar.kucharska@gmail.com>
2020-08-08 16:26:30 +0100

    Added: getting random phrases from the array, information - win/lose

--- wisielec.js
+++ wisielec.js
@@ -1,9 +1,12 @@
-var haslo = "Bez pracy nie ma kołaczy";
+
+var przyslowia = new Array("I króla robaki zjedzą","Im wcześniej tym lepiej","Paluszek i główka to szkolna wymówka","Do ludzi po rozum do matki po serce","Do grosza grosz to się napełni trzos","Cel uświęca środki","Co ma wisieć nie utonie","Czego oczy nie widzą tego sercu nie żal","Wszystko dobre co się dobrze kończy","Wszędzie dobrze gdzie nas nie ma","Hulaj dusza piekła nie ma","O pieniądzach się nie mówi pieniądze się ma","Łyżka dziegciu beczkę miodu zepsuje");
+var haslo = przyslowia[Math.ceil(Math.random()*przyslowia.length)];
 haslo = haslo.toUpperCase();
 
 var dlugosc = haslo.length;
 
 var haslozasloniete = "";
+var licznik_skuch = 0;
 
 for	(i=0; i<dlugosc; i++)
 {
@@ -55,6 +58,7 @@ litery[32]="Ń";
 litery[33]="Ś";
 litery[34]="V";
 
+
 function start()
 {
 	var tresc_diva = "";
@@ -72,13 +76,58 @@ function start()
 	wypisz_haslo();
 }
 
+String.prototype.ustawZnak = function(miejsce, znak)
+{
+	if (miejsce>this.length-1) return this.toString();
+	else return this.substr(0, miejsce) + znak + this.substr(miejsce+1);
+}
+
 function sprawdz(nr)
 {
+		var trafiona = false;
+		
 		for(i=0; i<dlugosc;i++)
 		{
 			if(haslo.charAt(i)==litery[nr])
 			{
-				alert(i);
+				haslozasloniete = haslozasloniete.ustawZnak(i, litery[nr]);	
+				trafiona = true;
 			}
 		}
+		
+		if(trafiona==true) 
+		{
+			var element = "lit" + nr;
+			document.getElementById(element).style.background="#003300";
+			document.getElementById(element).style.color="#00C000";
+			document.getElementById(element).style.border="3px solid #00C000";
+			document.getElementById(element).style.cursor = "context-menu";
+			
+			wypisz_haslo();
+		}
+		else 
+		{
+			var element = "lit" + nr;
+			document.getElementById(element).style.background="#330000";
+			document.getElementById(element).style.color="#c00000";
+			document.getElementById(element).style.border="3px solid #c00000";
+			document.getElementById(element).style.cursor = "context-menu";
+			document.getElementById(element).setAttribute("onclick",";");
+			licznik_skuch++;
+			var numer_obrazka = "img/s"+licznik_skuch+".jpg";
+			document.getElementById("szubienica").innerHTML='<img src='+numer_obrazka+' />';
+		}
+		
+		//wygrana
+		if(haslo == haslozasloniete)
+		{
+			document.getElementById("alfabet").innerHTML= "Wygrałeś! Hasło to: "+haslo+ ' </br></br> <span class="reset" onclick="location.reload()" > Chcesz zagrać ponownie?</span>'
+		}
+		
+		//przegrana
+		if(licznik_skuch>=9)
+		{
+			document.getElementById("alfabet").innerHTML= "Przegrałeś... Hasło to: " +haslo+ ' </br></br> <span class="reset" onclick="location.reload()" > Chcesz zagrać ponownie?</span>'
+		}
+	
 }
